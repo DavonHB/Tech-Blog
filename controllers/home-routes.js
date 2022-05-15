3636f6d45ef6f151a2e1ea9dfbf68e41951d69c2
Nosferatu <davonb113@gmail.com>
2022-05-15 18:53:02 -0500

    homepage render fix

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -30,7 +30,7 @@ router.get('/', (req, res) => {
     })
     .then(dbPostData => {
         const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('home-page', { posts, loggedIn: req.session.loggedIn });
+        res.render('homepage', { posts, loggedIn: req.session.loggedIn });
     })
     .catch(err => {
         console.log(err);
