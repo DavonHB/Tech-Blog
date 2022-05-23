5654dc762adb7cbc196098d08fe086618b1b5de0
Nosferatu <davonb113@gmail.com>
2022-05-23 12:39:21 -0500

    update

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -7,16 +7,10 @@ async function deleteFormHandler(event) {
       
       const response = await fetch(`/api/posts/${id}`, {
         method: 'DELETE',
-        body: JSON.stringify({
-          post_id: id
-        }),
-        headers: {
-          'Content-Type': 'application/json'
-        }
       });
       
       if (response.ok) {
-        document.location.replace('/dashboard/');
+        document.location.replace('/dashboard');
       } else {
         alert(response.statusText);
       }
